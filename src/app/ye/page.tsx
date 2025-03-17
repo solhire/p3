b4c6a0bdf23bbb13e195b1a6c570ed73cb68d4ec
yumsfun <yumsfun@proton.me>
2025-03-17 18:41:42 -0500

    Move hood image more to the left

--- src/app/ye/page.tsx
+++ src/app/ye/page.tsx
@@ -9,8 +9,8 @@ export default function Ye() {
         3.16.25
       </div>
       
-      {/* Hood image - bottom left - forcing rebuild */}
-      <div className="absolute bottom-6 left-6 w-32 h-32 md:w-48 md:h-48">
+      {/* Hood image - bottom left - moved left */}
+      <div className="absolute bottom-6 left-0 w-32 h-32 md:w-48 md:h-48">
         <img 
           src="/hood.png" 
           alt="Hood" 
